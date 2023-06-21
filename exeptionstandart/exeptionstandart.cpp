76f2654e7b8b5168b222335c959538322fa56732
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-06-21 15:54:16 +0700

    memberikan garis terakhir

--- exeptionstandart/exeptionstandart.cpp
+++ exeptionstandart/exeptionstandart.cpp
@@ -16,5 +16,8 @@ int main()
 	catch (exception& e) {
 		//penangkap menggunakan obyek exception
 		cout << e.what() << endl;
+		//*akan dieksekusi karna array data hanya memiliki 3 elemen*/
 	}
+	cout << "Baris Program Yang terakhir" << endl;
+
 }
